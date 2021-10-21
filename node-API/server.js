17a1cb14bb166c396028c934df5c48053478f98e
David Onaolapo <talk2dawich@yahoo.com>
2021-10-21 12:37:01 -0400

    Test GCP git actions

--- node-API/server.js
+++ node-API/server.js
@@ -60,7 +60,7 @@ app.get('/', (req, res) => {
   //   res.send('unique string')
   // })
   // console.log('test string')
-  res.send("HI, FOR GCPP LATEST")
+  res.send("HI, FOR GCPP AGAIN")
 })
 
 
